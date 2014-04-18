e95d8641acd4ca3fc2dc4340f3697968132261be
koyachi <rtk2106@gmail.com>
2014-04-18 22:50:43 +0900

    rewrite test.py with unittest.

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import mysqlqueryresult2dict
+import unittest
 
-DATA = '''mysql> SELECT * FROM foo;
+class TestParseText(unittest.TestCase):
+
+  def setUp(self):
+    self.data = '''mysql> SELECT * FROM foo;
 +----+-------+
 | id | value |
 +----+-------+
@@ -11,5 +15,17 @@ DATA = '''mysql> SELECT * FROM foo;
 3 rows in set (0.01 sec)
 '''
 
-dict = mysqlqueryresult2dict.parse_text(DATA)
-print dict
+  def test_parse_text(self):
+    result = mysqlqueryresult2dict.parse_text(self.data)
+    #print result
+    self.assertEqual(result['header'], ['mysql> SELECT * FROM foo;'], 'result["header"]')
+    self.assertEqual(result['fields'], ['id', 'value'], 'result["fields"]')
+    self.assertEqual(result['records'],
+                     [{'id': '1', 'value': 'A'},
+                      {'id': '2', 'value': 'b'},
+                      {'id': '3', 'value': 'C'}],
+                     'result["records"]')
+    self.assertEqual(result['footer'], ['3 rows in set (0.01 sec)', ''], 'result["footer"]')
+
+if __name__ == '__main__':
+  unittest.main()
